e77ff20b09033f3e42b8fc2a18e8588f228b0af9
Jonathan French <jaypeeff@gmail.com>
2024-05-21 12:20:07 +0300

    Initial Commit

---
+++ VoxelUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoxelUI
+//
+//  Created by Jonathan French on 21.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
